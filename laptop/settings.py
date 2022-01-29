2ab5cab0f457b4f18741071d4d00beb65924247e
Klemenz Hrafn <klemenz88@gmail.com>
2022-01-29 01:13:09 +0000

    Update settings

--- laptop/settings.py
+++ laptop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,69 +23,68 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config.secret_key
-SECURE_CONTENT_TYPE_NOSNIFF = config.env['SECURE_CONTENT_TYPE_NOSNIFF']
-SECURE_BROWSER_XSS_FILTER = config.env['SECURE_BROWSER_XSS_FILTER']
-SESSION_COOKIE_SECURE = config.env['SESSION_COOKIE_SECURE']
-CSRF_COOKIE_SECURE = config.env['CSRF_COOKIE_SECURE']
-X_FRAME_OPTIONS = config.env['X_FRAME_OPTIONS']
+SECURE_CONTENT_TYPE_NOSNIFF = config.env["SECURE_CONTENT_TYPE_NOSNIFF"]
+SECURE_BROWSER_XSS_FILTER = config.env["SECURE_BROWSER_XSS_FILTER"]
+SESSION_COOKIE_SECURE = config.env["SESSION_COOKIE_SECURE"]
+CSRF_COOKIE_SECURE = config.env["CSRF_COOKIE_SECURE"]
+X_FRAME_OPTIONS = config.env["X_FRAME_OPTIONS"]
 
-DEBUG = config.env['DEBUG']
+DEBUG = config.env["DEBUG"]
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '0.0.0.0',
-    '127.0.0.1',
-    'api.laptop.is',
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+    "api.laptop.is",
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
     "rest_framework_api_key",
-    'corsheaders',
-    'users',
-    'offerings',
-    'filters',
+    "corsheaders",
+    "users",
+    "offerings",
+    "filters",
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
-        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
     ),
-
-    'DEFAULT_PARSER_CLASSES': (
-        'djangorestframework_camel_case.parser.CamelCaseFormParser',
-        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
     ),
-    'JSON_UNDERSCOREIZE': {
-        'no_underscore_before_number': True,
+    "JSON_UNDERSCOREIZE": {
+        "no_underscore_before_number": True,
     },
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'laptop.custom_middleware.CamelCaseMiddleWare',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "laptop.custom_middleware.CamelCaseMiddleWare",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-AUTH_USER_MODEL = 'users.User' 
+AUTH_USER_MODEL = "users.User"
 
-ROOT_URLCONF = 'laptop.urls'
+ROOT_URLCONF = "laptop.urls"
 
 AWS_STORAGE_BUCKET_NAME = config.AWS_STORAGE_BUCKET_NAME
 AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
@@ -92,48 +92,46 @@ AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
 AWS_S3_CUSTOM_DOMAIN = config.AWS_S3_CUSTOM_DOMAIN
 AWS_S3_ENDPOINT_URL = config.AWS_S3_ENDPOINT_URL
 
-DEFAULT_FILE_STORAGE = 'laptop.storages.MediaStorage'
-MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = "laptop.storages.MediaStorage"
+MEDIAFILES_LOCATION = "media"
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'laptop.wsgi.application'
+WSGI_APPLICATION = "laptop.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config.postgres['NAME'],
-        'USER': config.postgres['USER'],
-        'PASSWORD': config.postgres['PASSWORD'],
-        'HOST': config.postgres['HOST'],
-        'PORT': config.postgres['PORT']
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config.postgres["NAME"],
+        "USER": config.postgres["USER"],
+        "PASSWORD": config.postgres["PASSWORD"],
+        "HOST": config.postgres["HOST"],
+        "PORT": config.postgres["PORT"],
     }
 }
 
-#CORS_ORIGIN_ALLOW_ALL = True
-
 CORS_ORIGIN_WHITELIST = [
-    'https://localhost:3000',
-    'https://laptop.is',
+    "https://localhost:3000",
+    "https://laptop.is",
 ]
 
 
@@ -142,16 +140,16 @@ CORS_ORIGIN_WHITELIST = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -159,9 +157,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -173,5 +171,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
